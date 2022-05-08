fdd50d3069d66abc6e6fc6a44a9f393d0c901b57
reejung.kim <reejung.kim@gmail.com>
2022-05-08 21:05:53 +0900

    Remove markdowns

---
+++ functions.py
@@ -0,0 +1,26 @@
+
+
+class FeatureSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, feature_names):
+        self.feature_names = feature_names
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        return X.loc[:, self.feature_names].copy(deep=True)
+
+
+num_pipeline = Pipeline(steps=[
+    ("num_selector", FeatureSelector(num_x_training)),
+    ("imputer", SimpleImputer(strategy="median")),
+    ("std_scaler", StandardScaler())
+])
+
+
+# Defining the steps in the categorical pipeline
+cat_pipeline = Pipeline([
+    ('cat_selector', FeatureSelector(cat_x_training)),
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('one_hot_encoder', OneHotEncoder(sparse=False, handle_unknown='ignore'))
+])
